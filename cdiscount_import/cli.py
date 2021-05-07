Jan-Panasiam/cdiscount_import

commit 728a4ef923383ee72389d3e8878ff449742243bf

Refactor: Improve image handling
The previous implementation always took the first and the last image,
but dismissed every other picture. Adjust the implementation to take as
many pictures as possible and at the last position if possible a swatch
image, which presents an overview of variations.

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

cdiscount_import/cli.py
@@ -4,6 +4,7 @@ import configparser
 import pandas as pd
 import numpy as np
 import openpyxl
+import re
 import os
 import requests
 from openpyxl.utils.dataframe import dataframe_to_rows
@@ -279,6 +280,52 @@ class PlentyFetch:
 
         return ''
 
+    def __get_images(self, variation : dict) -> list:
+        """
+        Get a maximum of 4 images for the Cdiscount columns.
+
+        The last image should always be a swatch image (an image that
+        represents multiple variations at once).
+
+        Parameters:
+            variation   [dict]  -   JSON of a single variation from the
+                                    Plentymarkets REST API
+
+        Return:
+                        [str]
+        """
+        try:
+            images = variation['images']
+        except KeyError:
+            return []
+
+        image_list = []
+        for image in images:
+            for availability in image['availabilities']:
+                if availability['value'] == self.referrer_id:
+                    image_list.append(
+                        {'url': image['url'], 'position': image['position']}
+                    )
+
+        if not image_list:
+            return []
+
+        image_list = sorted(image_list, key=lambda item: item.get('position'))
+        swatch_images = []
+        for index, image in enumerate(image_list):
+            # This is a highly specific condition for our use case
+            if re.search('swatch', image['url'].lower()):
+                swatch_images.append(image_list.pop(index))
+
+        if len(image_list) > 4 and len(swatch_images) == 0:
+            image_list = image_list[:4]
+        elif len(image_list) > 3 and len(swatch_images) >= 1:
+            image_list = image_list[:3] + [swatch_images[0]]
+        else:
+            image_list += swatch_images[:4-len(image_list)]
+
+        return [x['url'] for x in image_list]
+
     def extract_data(self):
         """
         Get all the variations from the API that have the referrerId of
@@ -376,35 +423,21 @@ class PlentyFetch:
 
             product_nature = 'Standard'
 
-            for image in variation['images']:
-                for availability in image['availabilities']:
-                    if availability['value'] == self.referrer_id:
-                        img = True
-
-                if img:
-                    image_block.append(image['url'])
-                    img = False
-
-            if image_block == []:
+            image_block = self.__get_images(variation=variation)
+            if not image_block:
                 err = True
                 image_block = ['No Image found']
 
+            data = [
+                seller_ref, barcode, brand, product_nature, category_id,
+                image_block[0], parent_sku, size, marketing_color
+            ] + image_block[1:]
             if err:
-                self.errors.append([
-                    seller_ref, barcode, brand, product_nature, category_id,
-                    image_block[0], parent_sku, size, marketing_color,
-                    image_block[-1]
-                ])
+                self.errors.append(data)
                 err = False
-                image_block = []
                 continue
 
-            self.variations.append([
-                seller_ref, barcode, brand, product_nature, category_id,
-                image_block[0], parent_sku, size, marketing_color,
-                image_block[-1]
-            ])
-            image_block = []
+            self.variations.append(data)
 
     def get_texts(self):
         """

commit 88165f84b4d90ebb55e3ab81b6ca5b721a4280a7

Fix: Adjust Error Excel file column headers
The headers were optimized for only two images, but the current
implementation supports 4 images. Adjust the headers accordingly.

Fixes: 728a4ef9 Refactor: Improve image handling

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

cdiscount_import/cli.py
@@ -655,8 +655,8 @@ class CdiscountWriter:
         ws.append([
             'seller_ref', 'barcode', 'brand', 'product_nature',
             'category_id', 'image_1', 'parent_sku', 'size', 'marketing_color',
-            'image_2', 'short_label', 'long_label', 'short_description',
-            'long_description'
+            'image_2', 'image_3', 'image_4', 'short_label', 'long_label',
+            'short_description', 'long_description'
         ])
         ws.append(['',''])
         for row in dataframe_to_rows(df, index = False, header = False):